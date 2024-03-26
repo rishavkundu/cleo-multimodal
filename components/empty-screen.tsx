5c7a32ab119d6ebf615250843ce4849d0a540e75
Rishav Kundu <rishavkundu@gmail.com>
2024-03-26 03:32:29 +0000

    footer changes

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -24,7 +24,7 @@ export function EmptyScreen() {
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
         <h1 className="text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Welcome to LeonAI Conversational Assistant!
         </h1>
         <p className="leading-normal text-muted-foreground">
           This is an open source AI chatbot app template built with{' '}
--- components/footer.tsx
+++ components/footer.tsx
@@ -12,10 +12,10 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
       )}
       {...props}
     >
-      Open source AI chatbot built with{' '}
-      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-      <ExternalLink href="https://github.com/vercel/ai">
-        Vercel AI SDK
+      Leon Assistant built with{' '}
+      <ExternalLink href="https://anthropic.com">Anthropic</ExternalLink> by{' '}
+      <ExternalLink href="https://rishavkundu.com">
+        Rishav Kundu
       </ExternalLink>
       .
     </p>
