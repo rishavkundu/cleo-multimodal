7e23662f06b538a0b68455d618dbdda74e15d79f
rishavkundu <rishavkundu@gmail.com>
2024-04-04 21:11:13 -0400

    added dev page

---
+++ app/dev/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DevPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Dev Page</h1>
+      <p>This is the dev page.</p>
+    </div>
+  );
+};
+
+export default DevPage;
